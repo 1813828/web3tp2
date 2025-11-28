af5b6f7cd415d78585cabb10a776ada8d2d8f93d
1813828 <89608322+1813828@users.noreply.github.com>
2025-11-28 02:16:45 -0500

    z dote mis

---
+++ src/js/zdog.js
@@ -0,0 +1,21 @@
+import Zdog from "https://esm.sh/zdog";
+
+const illo = new Zdog.Illustration({
+  element: ".zdog-canvas",
+  resize: true,
+  dragRotate: true // 👈
+});
+
+new Zdog.Ellipse({
+  addTo: illo,
+  diameter: 100,
+  stroke: 40,
+  color: "#4E9"
+});
+
+// 👇
+function animate() {
+  illo.updateRenderGraph();
+  requestAnimationFrame(animate);
+}
+animate();
