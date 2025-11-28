3cda6888f0f3b996ef64a4a1c2174e6e50915987
1813828 <89608322+1813828@users.noreply.github.com>
2025-11-28 01:04:49 -0500

    profile psychologique presque done

--- src/js/animejs.js
+++ src/js/animejs.js
@@ -15,3 +15,30 @@ chiffreElements.forEach((element) => {
     }
   });
 });
+//------------------------------animation  pour  les chiffres d'alertes --------------------------------------//
+
+
+const ringElements = svg.createDrawable(".ring");
+
+// Affichage du pourcentage dans le span
+const updateDrawLabel = () => {
+  ringElements.forEach((ringElement) => {
+    const drawValue = ringElement.getAttribute("draw");
+    const values = drawValue.trim().split(/\s+/); // Ex: "0 0.75"
+    const percent = Math.round(values[1] * 100);
+    ringElement
+      .closest(".circle-container")
+      .querySelector("span").textContent = `${percent}`;
+  });
+};
+
+animate(ringElements, {
+  draw: { from: "0 0", to: () => `0 ${utils.random(0, 1, 2)}` },
+  loop: true,
+  onUpdate: updateDrawLabel,
+  onLoop: (self) => {
+    self.refresh();
+    updateDrawLabel();
+  }
+});
+updateDrawLabel();
